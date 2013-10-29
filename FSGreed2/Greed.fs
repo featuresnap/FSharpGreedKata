9f4837b3fb26cad2a2c651012cb34c5043c7cff9
Patrick Kelly <patrick.kelly@sherwin.com>
2013-10-28 23:13:55 -0400

    Initial commit

---
+++ FSGreed2/Greed.fs
@@ -0,0 +1,74 @@
+﻿namespace GreedLib
+
+module Greed =
+    
+    type Die = int
+    type Score = int
+    type Dice = Die list
+    type MatchResult = (Score * Dice)
+    type ScoringRule = Dice -> MatchResult option 
+
+    let score = function |Some(score, dice) -> score |_ -> 0
+    let remainder = function |Some( _, dice) -> dice |_ -> []
+
+    let rec removeFirstOccurrence value list = 
+         match list with
+         |x :: xs when x = value ->  xs
+         |x :: xs -> x :: removeFirstOccurrence value xs
+         |_ -> []
+
+    let rec removeFirstNOccurrences value list n =
+        match list with 
+        |x::xs when x = value -> removeFirstNOccurrences value xs (n-1)
+        |x::xs -> x :: removeFirstNOccurrences value xs n
+        |_ -> []
+
+    let makeSingleDieMatchRule dieValue points : ScoringRule = fun dice ->
+        let found = dice |> List.exists ((=) dieValue)
+        match found with 
+        |true -> Some(points, dice |> removeFirstOccurrence dieValue)
+        |_ -> None
+
+    let makeMultiDiceMatchRule (dieValue, number) points : ScoringRule = fun dice -> 
+        let inThisGroup = 
+            dice 
+            |> Seq.groupBy id 
+            |> Seq.tryFind (fst >> ((=) dieValue)) 
+            |> Option.map (snd >> Seq.length)
+        match inThisGroup with 
+        |Some(n) when n = number -> 
+            Some(points, removeFirstNOccurrences(dieValue)(dice)(number))
+        |_ -> None
+
+    let evaluateRules dice rules = 
+        rules |> List.choose (fun rule -> rule dice) 
+
+    let rec generateScoreSets (dice:Dice) (rules:ScoringRule list) : MatchResult list list= 
+        [
+            let matches = evaluateRules dice rules
+            if matches |> List.length = 0 then yield []
+            for (score, remainder) in matches do
+            match remainder with 
+            |[] -> yield [(score, remainder)]
+            |_ -> for nextMove in generateScoreSets remainder rules do
+                  yield (score, remainder)::nextMove
+        ]     
+
+    let findBestScore dice rules = 
+        generateScoreSets dice rules
+        |> List.map(List.sumBy fst)
+        |> List.max
+        
+          
+    let matchSingleOne : ScoringRule = makeSingleDieMatchRule 1 100
+    let matchSingleFive : ScoringRule = makeSingleDieMatchRule 5 50
+    let matchThreeOnes : ScoringRule = makeMultiDiceMatchRule (1,3) 1000
+    let matchThreeTwos : ScoringRule = makeMultiDiceMatchRule (2,3) 200
+    let matchThreeThrees : ScoringRule = makeMultiDiceMatchRule (3,3) 300
+    let matchThreeFours : ScoringRule = makeMultiDiceMatchRule (4,3) 400
+    let matchThreeFives : ScoringRule = makeMultiDiceMatchRule (5,3) 500
+    let matchThreeSixes : ScoringRule = makeMultiDiceMatchRule (6,3) 600
+
+    let standardRules = [matchSingleOne; matchSingleFive; matchThreeOnes;
+                    matchThreeTwos; matchThreeThrees; matchThreeFours ;
+                    matchThreeFives; matchThreeSixes ]
---
+++ GreedTests/GreedTests.fs
@@ -0,0 +1,69 @@
+﻿namespace GreedTests
+
+module GreedTests =
+    open FsUnit
+    open Xunit
+    open GreedLib.Greed
+
+    let [<Fact>] ``Removing first occurrence of a die value leaves other occurrences with that value`` () =
+        [1;2;1] |> removeFirstOccurrence 1 |> should equal [2;1]
+
+    let [<Fact>] ``Single one rule scores 100`` () =
+        matchSingleOne [1] |> score |> should equal 100
+
+    let [<Fact>] ``Single one rule removes first one`` () =
+        matchSingleOne [1;2] |> remainder |> should equal [2]
+
+    let [<Fact>] ``Single five rule scores 50`` () =
+        matchSingleFive [5] |> score |> should equal 50
+
+    let [<Fact>] ``Single five rule removes first five`` () =
+        matchSingleFive [2;5] |> remainder |> should equal [2]
+
+    let [<Fact>] ``Single five rule does not match two ones`` () = 
+        matchSingleFive [1;1] |> Option.isNone |> should be True
+
+    let [<Fact>] ``Triple 1 rule scores 1000`` () = 
+        matchThreeOnes [1; 1; 1] |> score |> should equal 1000
+
+    let [<Fact>] ``Triple 1 rule removes first three ones`` () =
+        matchThreeOnes [1;2;1;5;1] |> remainder |> should equal [2;5]
+    
+    let [<Fact>] ``Triple 1 rule does not match two ones`` () = 
+        matchThreeOnes [1;1] |> Option.isNone |> should be True
+
+    let [<Fact>] ``Find matching rules finds all rules that match`` () = 
+        let rules = [matchSingleOne; matchSingleFive; matchThreeOnes]
+        let matchingRules = evaluateRules [1;1;5;1] rules
+        matchingRules |> List.exists((=) (1000,[5])) |> should be True
+        matchingRules |> List.exists((=) (100, [1; 5; 1])) |> should be True
+        matchingRules |> List.exists((=) (50, [1; 1; 1])) |> should be True
+
+    let [<Fact>] ``Generate score sets scores three ones as singles and a triple`` () =
+        let dice = [1; 1; 1]
+        let rules = [matchSingleOne; matchThreeOnes]
+        let waysToScore = generateScoreSets dice rules
+        waysToScore |> List.length |> should equal 2
+        waysToScore |> List.exists (fun matches -> matches |> List.sumBy fst = 300) |> should be True
+        waysToScore |> List.exists (fun matches -> matches |> List.sumBy fst = 1000) |> should be True
+    
+    let [<Fact>] ``Highest score finds highest score`` () =
+        let dice = [1; 1; 1]
+        let rules = [matchSingleOne; matchThreeOnes]
+        let bestScore = findBestScore dice rules
+        bestScore |> should equal 1000
+
+    let [<Fact>] ``Standard Scenarios`` () = 
+        standardRules |> findBestScore  [1; 1; 1; 5; 1] |> should equal  1150
+        standardRules |> findBestScore  [2; 3; 4; 6; 2] |> should equal  0   
+        standardRules |> findBestScore  [3; 4; 5; 3; 3] |> should equal  350 
+        standardRules |> findBestScore  [1; 5; 1; 2; 4] |> should equal  250 
+        standardRules |> findBestScore  [5; 5; 5; 5; 5] |> should equal  600
+
+
+
+
+
+
+
+
